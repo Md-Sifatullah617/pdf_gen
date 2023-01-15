28de1936f79c4b480f91bf1b9f1da049a92c486f
Md. Sifatullah <sifatullah35-617@diu.edu.bd>
2023-01-15 15:53:42 +0600

    login added

--- lib/auth/login.dart
+++ lib/auth/login.dart
@@ -10,12 +10,53 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
+    ButtonStyle buttonStyle =
+        ElevatedButton.styleFrom(minimumSize: const Size(double.infinity, 50));
     return Scaffold(
-        appBar: AppBar(
-            automaticallyImplyLeading: false,
-            title: const Text("Login"),
-            centerTitle: true,
-        ),
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        title: const Text("Login"),
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(25.0),
+        child: Column(children: [
+          const TextField(
+            decoration: InputDecoration(
+                labelText: "Email/Phone number", border: OutlineInputBorder()),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          const TextField(
+            decoration: InputDecoration(
+                labelText: "Password", border: OutlineInputBorder()),
+          ),
+          const SizedBox(
+            height: 50,
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            style: buttonStyle,
+            child: const Text("Login"),
+          ),
+          const SizedBox(height: 10,),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text("Don't have any account? "),
+              InkWell(
+                onTap: () {},
+                child: const Text(
+                  "Signup",
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold, color: Colors.blue),
+                ),
+              )
+            ],
+          )
+        ]),
+      ),
     );
   }
 }
