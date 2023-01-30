b1e1adafbe6c402c307d825703390c97a384e9d6
Md. Sifatullah <sifatullah35-617@diu.edu.bd>
2023-01-30 20:55:26 +0600

    logout event

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-
+import 'package:pdf_gen/auth/login.dart';
+import 'package:pdf_gen/utilities/utilities.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
@@ -10,42 +11,45 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  final _auth = FirebaseAuth.instance;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-            actions: [
-                IconButton(onPressed: () async {
-                    await FirebaseAuth.instance.signOut();
-                }, icon: const Icon(Icons.exit_to_app))
-            ],
-            automaticallyImplyLeading: true,
-            title: const Text("PDF_gen2.0"),
-            centerTitle: true,
-        ),
-        drawer: Drawer(
-            child: ListView(
-                children: const [
-                    UserAccountsDrawerHeader(accountName: Text("Md. Sifatullah"), accountEmail: Text("sifatullahsanowar1@gmail.com"),
-                    currentAccountPicture: CircleAvatar(
-                        backgroundImage: AssetImage("assets/images/dp.jpg")),
-                    ),
-                    ListTile(
-                        leading: Icon(Icons.wallet),
-                        title: Text("Donate us"),
-                    ),
-                    ListTile(
-                        leading: Icon(Icons.info),
-                        title: Text("About us"),
-                    ),
-                ],
+      appBar: AppBar(
+        actions: [
+          IconButton(
+              onPressed: () {
+                _auth.signOut().then((value) => Navigator.push(context, MaterialPageRoute(builder: (context)=>const LoginPage()))).onError((error, stackTrace) => Utilities().toastMessage(error.toString(), color: Colors.red));
+              },
+              icon: const Icon(Icons.exit_to_app))
+        ],
+        automaticallyImplyLeading: true,
+        title: const Text("PDF_gen2.0"),
+        centerTitle: true,
+      ),
+      drawer: Drawer(
+        child: ListView(
+          children: const [
+            UserAccountsDrawerHeader(
+              accountName: Text("Md. Sifatullah"),
+              accountEmail: Text("sifatullahsanowar1@gmail.com"),
+              currentAccountPicture: CircleAvatar(
+                  backgroundImage: AssetImage("assets/images/dp.jpg")),
             ),
+            ListTile(
+              leading: Icon(Icons.wallet),
+              title: Text("Donate us"),
+            ),
+            ListTile(
+              leading: Icon(Icons.info),
+              title: Text("About us"),
+            ),
+          ],
         ),
-        body: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: const [
-            Text("Congo!")
-        ]),
+      ),
+      body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const [Text("Congo!")]),
     );
   }
 }
