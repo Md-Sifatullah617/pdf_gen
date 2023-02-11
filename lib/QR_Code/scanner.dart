a091e0c63b3d0a30f29eafa430da699277b6ced7
Md. Sifatullah <sifatullah35-617@diu.edu.bd>
2023-02-11 13:58:27 +0600

    df

--- lib/QR_Code/scanner.dart
+++ lib/QR_Code/scanner.dart
@@ -9,47 +9,62 @@ class ScannerPdf extends StatefulWidget {
 }
 
 class _ScannerPdfState extends State<ScannerPdf> {
+  bool isScanComplete = false;
+  void closeScan() {
+    isScanComplete = false;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-            title: const Text("Scanner"),
-            centerTitle: true,
-        ),
-        body: Column(
-            children: [
-                Expanded(child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: const [
-                        Text("Place the QR code in the area", style: TextStyle(fontWeight: FontWeight.bold,
-                        fontSize: 18,
-                        letterSpacing: 1),),
-                        SizedBox(height: 10,),
-                        Text("Scanning will start automatically",
-                        style: TextStyle(
-                            fontSize: 16,
-                            color: Colors.black54,
-                        ),)
-                    ],
-                )),
-                Expanded(
-                    flex: 4,
-                    child: Padding(
-                      padding: const EdgeInsets.all(25.0),
-                      child: MobileScanner(onDetect: (barcodes) {
-                        
-                      },),
-                    ),),
-                Expanded(child: Container(
-                    alignment: Alignment.center,
-                    child: const Text("Developed by Md. Sifatullah",
-                    style: TextStyle(
-                        fontSize: 16,
-                        color: Colors.black54
-                    ),),
-                )),
+      appBar: AppBar(
+        title: const Text("Scanner"),
+        centerTitle: true,
+      ),
+      body: Column(
+        children: [
+          Expanded(
+              child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: const [
+              Text(
+                "Place the QR code in the area",
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18,
+                    letterSpacing: 1),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Text(
+                "Scanning will start automatically",
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.black54,
+                ),
+              )
             ],
-        ),
+          )),
+          Expanded(
+            flex: 4,
+            child: Padding(
+              padding: const EdgeInsets.all(25.0),
+              child: MobileScanner(
+                onDetect: (barcodes) {},
+              ),
+            ),
+          ),
+          Expanded(
+              child: Container(
+            alignment: Alignment.center,
+            child: const Text(
+              "Developed by Md. Sifatullah",
+              style: TextStyle(fontSize: 16, color: Colors.black54),
+            ),
+          )),
+        ],
+      ),
     );
   }
 }
