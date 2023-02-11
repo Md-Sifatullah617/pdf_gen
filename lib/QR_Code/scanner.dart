820265ebcebb3030803039c80c5985995d4d1409
Md. Sifatullah <sifatullah35-617@diu.edu.bd>
2023-02-11 12:23:51 +0600

    fs

--- lib/QR_Code/scanner.dart
+++ lib/QR_Code/scanner.dart
@@ -15,6 +15,23 @@ class _ScannerPdfState extends State<ScannerPdf> {
             title: const Text("Scanner"),
             centerTitle: true,
         ),
+        body: Column(
+            children: [
+                Expanded(child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: const [
+                        Text("Place the QR code in the area", style: TextStyle(fontWeight: FontWeight.bold,
+                        fontSize: 18,
+                        letterSpacing: 1),),
+                        Text("Scanning will start automatically")
+                    ],
+                )),
+                Expanded(
+                    flex: 4,
+                    child: Container(color: Colors.green,)),
+                Expanded(child: Container(color: Colors.blue,)),
+            ],
+        ),
     );
   }
 }
