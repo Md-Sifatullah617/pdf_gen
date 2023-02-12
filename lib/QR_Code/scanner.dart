50bd0f146f49b61c3fcf0bcf589bae3aea09c611
Md. Sifatullah <sifatullah35-617@diu.edu.bd>
2023-02-12 18:32:01 +0600

    sdfd

--- lib/QR_Code/scanner.dart
+++ lib/QR_Code/scanner.dart
@@ -9,13 +9,17 @@ class ScannerPdf extends StatefulWidget {
 }
 
 class _ScannerPdfState extends State<ScannerPdf> {
-  final txtController = TextEditingController();
   int seletedItem = 0;
   void onItemTapped(int index) {
     setState(() {
       seletedItem = index;
     });
   }
+
+  static const List widgetsOption = [
+    Generator(),
+    ScannerF()
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -25,61 +29,140 @@ class _ScannerPdfState extends State<ScannerPdf> {
         centerTitle: true,
       ),
       bottomNavigationBar: BottomNavigationBar(
-        currentIndex: seletedItem,
-        onTap: onItemTapped,
-        items: const [
-        BottomNavigationBarItem(icon: Icon(Icons.scanner), label: "generator",
-        
-        ),
-        BottomNavigationBarItem(icon: Icon(Icons.qr_code), label: "Scanner"),
-      ]),
+          currentIndex: seletedItem,
+          onTap: onItemTapped,
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.scanner),
+              label: "generator",
+            ),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.qr_code), label: "Scanner"),
+          ]),
       body: Padding(
-        padding: const EdgeInsets.all(25.0),
-        child: Column(
-          children: [
-            Expanded(
-              child: BarcodeWidget(
-                  data: txtController.text,
-                  color: Colors.red,
-                  width: 200,
-                  height: 200,
-                  barcode: Barcode.qrCode()),
-            ),
-            const SizedBox(
-              height: 40,
-            ),
-            Row(
-              children: [
-                Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 20),
-                    child: TextField(
-                      controller: txtController,
-                      decoration:
-                          const InputDecoration(border: OutlineInputBorder()),
-                    ),
+          padding: const EdgeInsets.all(25.0),
+          child: widgetsOption.elementAt(seletedItem)),
+    );
+  }
+}
+
+class Generator extends StatefulWidget {
+  const Generator({super.key});
+
+  @override
+  State<Generator> createState() => _GeneratorState();
+}
+
+class _GeneratorState extends State<Generator> {
+  final txtController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Expanded(
+            child: BarcodeWidget(
+                data: txtController.text,
+                color: Colors.red,
+                width: 200,
+                height: 200,
+                barcode: Barcode.qrCode()),
+          ),
+          const SizedBox(
+            height: 40,
+          ),
+          Row(
+            children: [
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: TextField(
+                    controller: txtController,
+                    decoration:
+                        const InputDecoration(border: OutlineInputBorder()),
                   ),
                 ),
-                FloatingActionButton(
-                  onPressed: () {
-                    setState(() {});
-                  },
-                  backgroundColor: Theme.of(context).primaryColor,
-                  child: const Icon(Icons.done),
-                ),
-              ],
-            ),
-            Expanded(
-              child: Container(
-                alignment: Alignment.center,
-                child: const Text(
-                  "Developed by Md. Sifatullah",
-                  style: TextStyle(fontSize: 16, color: Colors.black54),
-                ),
+              ),
+              FloatingActionButton(
+                onPressed: () {
+                  setState(() {});
+                },
+                backgroundColor: Theme.of(context).primaryColor,
+                child: const Icon(Icons.done),
+              ),
+            ],
+          ),
+          Expanded(
+            child: Container(
+              alignment: Alignment.center,
+              child: const Text(
+                "Developed by Md. Sifatullah",
+                style: TextStyle(fontSize: 16, color: Colors.black54),
               ),
             ),
-          ],
-        ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ScannerF extends StatefulWidget {
+  const ScannerF({super.key});
+
+  @override
+  State<ScannerF> createState() => _ScannerFState();
+}
+
+class _ScannerFState extends State<ScannerF> {
+  final txtController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Expanded(
+            child: BarcodeWidget(
+                data: txtController.text,
+                color: Colors.red,
+                width: 200,
+                height: 200,
+                barcode: Barcode.qrCode()),
+          ),
+          const SizedBox(
+            height: 40,
+          ),
+          Row(
+            children: [
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: TextField(
+                    controller: txtController,
+                    decoration:
+                        const InputDecoration(border: OutlineInputBorder()),
+                  ),
+                ),
+              ),
+              FloatingActionButton(
+                onPressed: () {
+                  setState(() {});
+                },
+                backgroundColor: Theme.of(context).primaryColor,
+                child: const Icon(Icons.done),
+              ),
+            ],
+          ),
+          Expanded(
+            child: Container(
+              alignment: Alignment.center,
+              child: const Text(
+                "Developed by Md. Sifatullah",
+                style: TextStyle(fontSize: 16, color: Colors.black54),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
