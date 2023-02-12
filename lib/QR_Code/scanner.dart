696e2998f1673f1dc6aceb25da1734af9e1401d6
Md. Sifatullah <sifatullah35-617@diu.edu.bd>
2023-02-12 16:50:04 +0600

    df

--- lib/QR_Code/scanner.dart
+++ lib/QR_Code/scanner.dart
@@ -19,46 +19,6 @@ class _ScannerPdfState extends State<ScannerPdf> {
       appBar: AppBar(
         title: const Text("Scanner"),
         centerTitle: true,
-        actions: [
-          IconButton(
-              onPressed: () {
-                cameraController.toggleTorch();
-              },
-              color: Colors.white,
-              iconSize: 32,
-              icon: ValueListenableBuilder(
-                  valueListenable: cameraController.torchState,
-                  builder: (context, state, child) {
-                    switch (state as TorchState) {
-                      case TorchState.off:
-                        return const Icon(
-                          Icons.flash_off,
-                          color: Colors.grey,
-                        );
-                      case TorchState.on:
-                        return const Icon(
-                          Icons.flash_on,
-                          color: Colors.yellow,
-                        );
-                    }
-                  })),
-          IconButton(
-            color: Colors.white,
-            icon: ValueListenableBuilder(
-              valueListenable: cameraController.cameraFacingState,
-              builder: (context, state, child) {
-                switch (state as CameraFacing) {
-                  case CameraFacing.front:
-                    return const Icon(Icons.camera_front);
-                  case CameraFacing.back:
-                    return const Icon(Icons.camera_rear);
-                }
-              },
-            ),
-            iconSize: 32.0,
-            onPressed: () => cameraController.switchCamera(),
-          ),
-        ],
       ),
       body: Column(
         children: [
