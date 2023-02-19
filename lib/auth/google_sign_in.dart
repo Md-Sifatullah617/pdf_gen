8697ae7d49f0bdc7c1f6bb997dcae201c9e69978
Md-Sifatullah617 <sifatullah35-617@diu.edu.bd>
2023-02-19 19:46:20 +0600

    new file

---
+++ lib/auth/google_sign_in.dart
@@ -0,0 +1,9 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+class GoogleSignInProvider extends ChangeNotifier {
+  final googleSignIn = GoogleSignIn();
+  GoogleSignInAccount? _user;
+  GoogleSignInAccount get user => _user!;
+}
