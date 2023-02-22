c1d329511cd4f2de417f96fa80e40d4d6e8561e0
Md-Sifatullah617 <sifatullah35-617@diu.edu.bd>
2023-02-23 01:38:41 +0600

    sdfa

--- lib/auth/google_sign_in.dart
+++ lib/auth/google_sign_in.dart
@@ -1,25 +1,30 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:pdf_gen/utilities/utilities.dart';
 
 class GoogleSignInProvider {
   final _googleSignIn = GoogleSignIn();
 
   //signInWithGoogle
-  signInWithGoogle() async {
-    //trigger the authentication flow
-    final GoogleSignInAccount? googleUser =
-        await GoogleSignIn(scopes: <String>['email']).signIn();
+  Future signInWithGoogle(context) async {
+    try {
+      //trigger the authentication flow
+      final GoogleSignInAccount? googleUser = await _googleSignIn.signIn();
 
-    //obtain the auth details from the request
-    final GoogleSignInAuthentication googleAuth =
-        await googleUser!.authentication;
-
-    //create new credential
-    final credential = GoogleAuthProvider.credential(
-        accessToken: googleAuth.accessToken, idToken: googleAuth.idToken);
-
-    //once signed in return the userCredential
-    return await FirebaseAuth.instance.signInWithCredential(credential);
+      if (googleUser != null) {
+        //obtain the auth details from the request
+        final GoogleSignInAuthentication googleAuth =
+            await googleUser.authentication;
+        //create new credential
+        final credential = GoogleAuthProvider.credential(
+            accessToken: googleAuth.accessToken, idToken: googleAuth.idToken);
+        //once signed in return the userCredential
+        return await FirebaseAuth.instance.signInWithCredential(credential);
+      }
+    } on FirebaseAuthException catch (e) {
+      Utilities().toastMessage(e.toString(), color: Colors.red);
+    }
   }
   //signout
 }
